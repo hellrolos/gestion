9b5fd120c96fe0f19cfafc7c02eca431cbaaaed3
rvillalobos <rvillalobos@ittepic.edu.mx>
2019-03-25 19:00:37 -0700

    Se realizo el cambio de plantilla en el login y tambien los mensajes de error fueron colocados en la libreria toastrjs

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,85 +1,62 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
     <title>Acceso | Gestión de Curso</title>
-    <meta content="" name="description" />
-    <meta content="" name="author" />
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-    <link href={{ asset('template/plugins/jquery-ui/jquery-ui.min.css') }} rel="stylesheet" />
-    <link href={{ asset('template/plugins/bootstrap/4.0.0/css/bootstrap.min.css') }} rel="stylesheet" />
-    <link href={{ asset('template/plugins/font-awesome/5.0/css/fontawesome-all.min.css') }} rel="stylesheet" />
-    <link href={{ asset('template/plugins/animate/animate.min.css') }} rel="stylesheet" />
-    <link href={{ asset('template/css/default/style.min.css') }} rel="stylesheet" />
-    <link href={{ asset('template/css/default/style-responsive.min.css') }} rel="stylesheet" />
-    <link href={{ asset('template/css/default/theme/default.css" rel="stylesheet') }} id="theme" />
-    <link rel="stylesheet" href={{ asset('css/toastr.min.css')}}>
-    <script src={{ asset('template/plugins/pace/pace.min.js') }}></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="icon" type="image/png" href={{ asset('prueba/images/favicon.ico')}}/>
+    <link rel="stylesheet" type="text/css" href={{ asset('prueba/css/bootstrap.min.css')}}/>
+    <link rel="stylesheet" type="text/css" href={{ asset('prueba/css/font-awesome.min.css')}}/>
+    <link rel="stylesheet" type="text/css" href={{ asset('prueba/css/icon-font.min.css')}}/>
+    <link rel="stylesheet" type="text/css" href={{ asset('prueba/css/animate.css')}}/>
+    <link rel="stylesheet" type="text/css" href={{ asset('prueba/css/hamburgers.min.css')}}/>
+    <link rel="stylesheet" type="text/css" href={{ asset('prueba/css/animsition.min.css')}}/>
+    <link rel="stylesheet" type="text/css" href={{ asset('prueba/css/select2.min.css')}}/>
+    <link rel="stylesheet" type="text/css" href={{ asset('prueba/css/daterangepicker.css')}}/>
+    <link rel="stylesheet" type="text/css" href={{ asset('prueba/css/util.css')}}/>
+    <link rel="stylesheet" type="text/css" href={{ asset('prueba/css/main.css')}}/>
+    <link rel="stylesheet" href={{ asset('css/toastr.min.css')}}/>
 </head>
-<body class="pace-top">
-    <!-- begin #page-loader -->
-    <div id="page-loader" class="fade show"><span class="spinner"></span></div>
-    <!-- end #page-loader -->
-    <div class="login-cover">
-        <div class="login-cover-image" style="background-image: url(template/img/login-bg/login-bg-12.jpg)" data-id="login-cover-image"></div>
-        <div class="login-cover-bg"></div>
-    </div>
-     <hr>
-    <div id="page-container" class="fade">
-        <!-- begin login -->
-        <div class="login login-v2" data-pageload-addclass="animated fadeIn">
-            <!-- begin brand -->
-            <div class="login-header">
-                <div class="brand">
-                    <span class="logo"></span> <b>TecNM</b> ITTepic
-                    <small>Gestión de Curso</small>
+<body>
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <div class="login100-form-title" style="background-image: url(prueba/images/bg-01.jpg);">
+                    <span class="login100-form-title-1">
+                        Gestión de Curso
+                    </span>
                 </div>
-                <div class="icon">
-                    <i class="fa fa-lock"></i>
-                </div>
-            </div>
-            <!-- end brand -->
-            <!-- begin login-content -->
-            <div class="login-content">
-                <form action="{{ route('login')}}" method="POST" class="margin-bottom-0">
+                <form class="login100-form validate-form" action="{{ route('login')}}" method="POST">
                     {{ csrf_field() }}
-                    <div class="form-group m-b-20">
-                        <input type="text" name="username"  value="{{ old('username')}}" class="form-control form-control-lg" placeholder="Usuario"/>
-                        {!! $errors->first('username', '<span class="hljs-string" style="color:white;">¡:message!</span>') !!}
+                    <div class="wrap-input100 validate-input m-b-26">
+                        <span class="label-input100">Usuario</span>
+                        <input class="input100" type="text" name="username" value="{{ old('username')}}" placeholder="Ingresar usuario"/>
                     </div>
-                    <div class="form-group m-b-20">
-                        <input type="password" name="password" class="form-control form-control-lg" placeholder="Contraseña"/>
-                        {!! $errors->first('password', '<span class="hljs-string" style="color:white;">¡:message!</span>') !!}
+                    <div class="wrap-input100 validate-input m-b-18">
+                        <span class="label-input100">Contraseña</span>
+                        <input class="input100" type="password" name="password" placeholder="Ingresar contraseña"/>
                     </div>
-                    <div class="login-buttons">
-                        <button type="submit" class="btn btn-success btn-block btn-lg">Acceder al sitio</button>
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn" type="submit">
+                            Acceder al sitio
+                        </button>
                     </div>
                 </form>
             </div>
-            <!-- end login-content -->
         </div>
-        <!-- end login -->
     </div>
-    <!-- ================== BEGIN BASE JS ================== -->
-    <script src={{ asset('template/plugins/jquery/jquery-3.2.1.min.js') }}></script>
-    <script src={{ asset('template/plugins/jquery-ui/jquery-ui.min.js') }}></script>
-    <script src={{ asset('template/plugins/bootstrap/4.0.0/js/bootstrap.bundle.min.js') }}></script>
-    <script src={{ asset('template/plugins/slimscroll/jquery.slimscroll.min.js') }}></script>
-    <script src={{ asset('template/plugins/js-cookie/js.cookie.js') }}></script>
-    <script src={{ asset('template/js/theme/default.min.js') }}></script>
-    <script src={{ asset('template/js/apps.min.js') }}></script>
-    <!-- ================== END BASE JS ================== -->
-    <!-- ================== BEGIN PAGE LEVEL JS ================== -->
-    <script src={{ asset('template/js/demo/login-v2.demo.min.js') }}></script>
+    <script src={{ asset('prueba/js/jquery-3.2.1.min.js')}}></script>
+    <script src={{ asset('prueba/js/animsition.min.js')}}></script>
+    <script src={{ asset('prueba/js/popper.js')}}></script>
+    <script src={{ asset('prueba/js/bootstrap.min.js')}}></script>
+    <script src={{ asset('prueba/js/select2.min.js')}}></script>
+    <script src={{ asset('prueba/js/moment.min.js')}}></script>
+    <script src={{ asset('prueba/js/daterangepicker.js')}}></script>
+    <script src={{ asset('prueba/js/countdowntime.js')}}></script>
+    <script src={{ asset('prueba/js/main.js')}}></script>
     <script src={{ asset('js/toastr.min.js')}}></script>
-    <!-- ================== END PAGE LEVEL JS ================== -->
-    <script>
-        $(document).ready(function() {
-            App.init();
-            LoginV2.init();
-        });
-    </script>
+    {!! $errors->first('username', '<script>toastr.error(":message", "¡Oops!", {timeOut:5000, progressBar:true, positionClass: "toast-top-center"})</script>') !!}
+    {!! $errors->first('password', '<script>toastr.error(":message", "¡Oops!", {timeOut:5000, progressBar:true, positionClass: "toast-top-center"})</script>') !!}
     @if(session()->has('flash'))
         <script>toastr.error("{{ session('flash')}}", "¡Oops!", {timeOut:5000, progressBar:true, positionClass: 'toast-top-center'})</script>
     @elseif(session()->has('success'))
